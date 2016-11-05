67a9dfad18adc5d8297d554eb5aaa0681b219e57
jony <jonyustc15@gmail.com>
2016-11-06 01:11:51 +0600

    first commit

---
+++ database/migrations/2016_11_05_180822_create_student_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("student_name");
+            $table->string("father_name");
+            $table->string("mother_name");
+            $table->string("gender");
+            $table->date("dob");
+            $table->integer("class_id");
+            $table->integer("roll");
+            $table->string("address");
+            $table->integer("phone");
+            $table->string("email");
+            $table->string("password");
+            $table->binary("photo");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
